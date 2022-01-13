803f7c7c2d883f335bb76e65c380cb66f99ce182
mareadelmar <mar.ortega.v@gmail.com>
2022-01-13 17:51:05 -0300

    Create footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.container}>
+            el footer
+        </footer>
+    )
+}
+
+export default Footer;
