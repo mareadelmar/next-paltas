ced99794c7f6bafaf616d18fc34caf0bb673e884
mareadelmar <mar.ortega.v@gmail.com>
2022-01-13 17:53:31 -0300

    Style card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from "./card.module.css";
+import Link from "next/link"
 
-const Card = (product) => {
+const Card = ({name, id, image, price}:TProduct) => {
     return (
-        <div className={styles.container}>
-            <h3 className={styles.title}>{product.name}</h3>
-            <img src={product.image} alt={product.title} className={styles.img} />
-        </div>
+        <Link href={`/product/${id}`}>
+            <div className={styles.container}>
+                <h3 className={styles.title}>{name}</h3>
+                <img src={image} alt={name} className={styles.img} />
+                <div className={styles.price}>
+                    <p><strong>Price:</strong> {price}</p>
+                </div>
+            </div>
+        </Link>
     )
 }
 
