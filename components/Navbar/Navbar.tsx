3becb0db06ec4b5e3110c3015ad5c7060ae1eda7
mareadelmar <mar.ortega.v@gmail.com>
2022-01-13 16:49:14 -0300

    Style navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
 	return (
-		<nav>
-			<Link href='/'>
-				<a>Home</a>
-			</Link>
-			<Link href='/about'>
-				<a>About</a>
-			</Link>
-		</nav>
+		<div className={styles.container}>
+			{/* <nav className={styles.box}>
+				<Link href='/'>
+					<a>Home</a>
+				</Link>
+				<Link href='/about'>
+					<a>About</a>
+				</Link>
+			</nav> */}
+			<div className={styles.logoContainer}>
+				<img src="/images/avocado.png" alt="logo" className={styles.img}/>
+				<h1 className={styles.title}>avos</h1>
+			</div>
+			<div className={styles.cartContainer}>
+				<p>carrito</p>
+			</div>
+		</div>
 	);
 };
 
