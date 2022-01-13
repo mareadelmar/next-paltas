7d7db1e1cbfad250a95ba49d79d968cc621376cf
mareadelmar <mar.ortega.v@gmail.com>
2022-01-13 10:13:02 -0300

    Add data fetching to detail page

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useRouter } from "next/router";
 
-const ProductItem = () => {
-	const router = useRouter();
+const ProductItem = ()  => {
+	const {query: { id}} = useRouter();
+	const [avo, setAvo] = useState<TProduct>()
+
+	useEffect(() => {
+		window.fetch(`/api/avo/${id}`)
+			.then(res => res.json())
+			.then(setAvo)
+	}, [])
 
 	return (
 		<div>
-			<h1>product item: {router.query.id}</h1>
+			<h1>product item: {avo?.name}</h1>
+			<img src={avo?.image} alt={avo?.name} />
 		</div>
 	);
 };
