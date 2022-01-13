392877f87a859a907ed8e3ab16b01ebed9fd75c0
mareadelmar <mar.ortega.v@gmail.com>
2022-01-13 18:48:13 -0300

    Create and style product detail page

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from "react";
 import { useRouter } from "next/router";
+import styles from "./productItem.module.css";
 
 const ProductItem = ()  => {
 	const {query: { id}} = useRouter();
 	const [avo, setAvo] = useState<TProduct>()
+	console.log(avo)
 
 	useEffect(() => {
 		window.fetch(`/api/avo/${id}`)
@@ -11,10 +13,20 @@ const ProductItem = ()  => {
 			.then(setAvo)
 	}, [])
 
+	const handleAddToCart = () => {
+		console.log("agregar al carrito")
+	}
+
+	if(!avo) return <p>Cargando...</p>
 	return (
-		<div>
-			<h1>product item: {avo?.name}</h1>
+		<div className={styles.container}>
+			<h2 className={styles.title}>{avo?.name}</h2>
 			<img src={avo?.image} alt={avo?.name} />
+			<div className={styles.text}>
+				<p><strong>Description: </strong> {avo?.attributes.description}</p>
+				<p><strong>Price: </strong>{avo?.price}</p>
+			</div>
+			<button onClick={handleAddToCart} className={styles.btn}>ADD TO CART</button>
 		</div>
 	);
 };
