a35c9dbb9177c9a9bf42bc1dd792fbbad8fc572e
mareadelmar <mar.ortega.v@gmail.com>
2022-01-17 10:57:51 -0300

    Fix card link to dynamic route

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 const Card = ({name, id, image, price}:TProduct) => {
     return (
-        <Link href={`/product/${id}`}>
+        <Link href={"/product/[id]"} as={`/product/${id}`}>
             <div className={styles.container}>
                 <h3 className={styles.title}>{name}</h3>
                 <img src={image} alt={name} className={styles.img} />
